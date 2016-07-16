b80d9e59b71334dffe4f9e63be029e7ac28c13f4
joeyism <sham.joey@gmail.com>
2016-07-16 01:53:42 -0400

    Added radio buttons for specific game

--- public/js/states/index.js
+++ public/js/states/index.js
@@ -10,16 +10,72 @@ var teams = [
 ];
 
 var TeamDropdown = React.createClass({
+    getInitialState: function(){
+        return {
+            url:""
+        }
+    },
+    getTeamData: function(){
+        var that = this;
+        $.ajax({
+            url: this.state.url,
+            type: "GET"
+        }).done(function(data){
+            that.displayGames(data);
+        }).fail(function(err){
+            console.log(err);
+        });
+    },
+    displayGames: function(data){
+        var gameRadioButtons = React.createClass({
+            render: function(){
+                var createRadioButton = function(obj, i){
+                    var label = new Date(obj.Year, obj.Month, obj.Day).toString().split(" ").splice(0, 4).join(" ") + " with " + obj.Away + " at " + obj.Home;
+                    return (
+                        <div key={i}>
+                            <input id={obj.GameId} type="radio" name="game" value={obj.GameId}/> {label}
+                        </div>
+                    )
+                };
+
+                return (
+                    <form id="game_radio_buttons" action="">
+                        {data.map(createRadioButton)} 
+                    </form>  
+                ) 
+            }
+        });
+        var gameRadioButtonElement= React.createElement(
+            gameRadioButtons, {}
+        );
+        ReactDOM.render(gameRadioButtonElement, document.getElementById("games"), function(){
+            $("#game_radio_buttons").click(function(){
+                var val = $("input[name='game']:checked", "#game_radio_buttons").val();
+                console.log(val);
+            });
+        });
+    },   
     selectFirstOption: function(e){
         var obj = {firstOption: e.target.value};
-        this.setState(obj);
+        var that = this;
+        this.setState(obj, function(){
+            this.setUrl();
+        });
     },
     selectSecondOption: function(e){
         var obj = {secondOption: e.target.value};
-        this.setState(obj);
+        var that = this;
+        this.setState(obj, function(){
+            that.setUrl();
+        });
     },
-    viewState: function(){
-        console.log(this.state);
+    setUrl: function(){
+        var that = this;
+        this.setState({url: "/api/" + this.state.firstOption + "/vs/" + this.state.secondOption }, function(){
+            if (this.state.firstOption !== undefined && this.state.secondOption !== undefined){
+                that.getTeamData.apply(that);
+            }
+        })
     },
     render: function(){
         var createSelect = function(obj, i){
@@ -28,22 +84,20 @@ var TeamDropdown = React.createClass({
 
         return(
             <div>
-                <div className="row">
-                    <div>
-                        <select className="col-md-2" id="select-teams-1" onChange={this.selectFirstOption}>
-                            <option value defaultValue>Select a team</option>
-                            {teams.map(createSelect)}
-                        </select>
-                    </div>
-                    <div>
-                        <select className="col-md-2" id="select-teams-2" onChange={this.selectSecondOption}>
-                            <option value defaultValue>Select a team</option>
-                            {teams.map(createSelect)}
-                        </select>
-                    </div>
-                    <div className="clearfix"/>
-                    <button className="row col-md-2 col-md-offset-1" onClick={this.viewState}>Get Data</button>
+                <div>
+                    <select className="col-md-2" id="select-teams-1" onChange={this.selectFirstOption}>
+                        <option value defaultValue>Select a team</option>
+                        {teams.map(createSelect)}
+                    </select>
                 </div>
+                <div>
+                    <select className="col-md-2" id="select-teams-2" onChange={this.selectSecondOption}>
+                        <option value defaultValue>Select a team</option>
+                        {teams.map(createSelect)}
+                    </select>
+                </div>
+                <div className="col-md-2" id="games"></div>
+                <div className="clearfix"/>
             </div>
         );
     }
